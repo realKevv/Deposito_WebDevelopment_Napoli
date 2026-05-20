35df504c7eaf8444371b85faf8d08a32e83bdec2
Kevin Napoli <kevinnapoli@outlook.it>
2026-05-20 15:04:31 +0200

    struttura Esercizio4

---
+++ Mercoledi_20-05_Napoli/ManipolazioneCSS/ESERCIZIO4/script.js
@@ -0,0 +1,26 @@
+
+let titolo = document.getElementById("titolo");
+let mioDiv = document.getElementById("mioDiv");
+let testoDiv = document.getElementById("testoDiv");
+let inputTesto = document.getElementById("inputTesto");
+
+let bottoneAggiungi = document.getElementById("bottoneAggiungi");
+let bottoneElimina = document.getElementById("bottoneElimina");
+let bottoneModificaStile = document.getElementById("bottoneModificaStile");
+
+
+let listaVuota = document.getElementById("listaVuota");
+
+/*evento1*/
+
+
+
+
+/*evento2*/
+
+
+
+
+/*evento3*/
+
+
