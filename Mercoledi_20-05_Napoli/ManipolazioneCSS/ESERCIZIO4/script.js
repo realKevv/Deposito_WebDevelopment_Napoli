f27af27f2604e264e1d47c5a1d6c65c59882723b
Kevin Napoli <kevinnapoli@outlook.it>
2026-05-20 16:11:02 +0200

    aggiunto extra: contatore automatico, eliminazione al click di ogni "li"

--- Mercoledi_20-05_Napoli/ManipolazioneCSS/ESERCIZIO4/script.js
+++ Mercoledi_20-05_Napoli/ManipolazioneCSS/ESERCIZIO4/script.js
@@ -11,20 +11,26 @@ let bottoneModificaStile = document.getElementById("bottoneModificaStile");
 
 let listaVuota = document.getElementById("listaVuota");
 
+let contatore = document.getElementById("contatore");
 
 /*evento1*/
 
 /*value per scrivere dentro un input/textarea, 
 innerHTML per scrivere dentro un div/p/span ecc...*/
-
 bottoneAggiungi.addEventListener("click", function () {
     let testoSalvato = inputTesto.value;
-    
+
     if (testoSalvato.trim() !== "") {
         let nuovoElemento = document.createElement("li");
         nuovoElemento.innerHTML = testoSalvato;
+
+        nuovoElemento.addEventListener("click", function () {
+            listaVuota.removeChild(nuovoElemento);
+
+            aggiornaContatore();
+        });
         listaVuota.appendChild(nuovoElemento);
-        
+
         // Pulizia dell'input dopo l'aggiunta
         inputTesto.value = "";
     }
@@ -35,29 +41,37 @@ bottoneAggiungi.addEventListener("click", function () {
 /*evento2*/
 
 bottoneElimina.addEventListener("click", function () {
-    if(listaVuota.children.length > 0) {
+    if (listaVuota.children.length > 0) {
         let ulttimoElemento = listaVuota.lastElementChild;
         listaVuota.removeChild(ulttimoElemento)
+
+        aggiornaContatore();
     }
 });
 
 /*evento3*/
-    bottoneModificaStile.addEventListener("click", function() {
-        titolo.style.color = "blue";
-        titolo.innerHTML = "NUOVO TITOLOOO";
+bottoneModificaStile.addEventListener("click", function () {
+    titolo.style.color = "blue";
+    titolo.innerHTML = "NUOVO TITOLOOO";
 
-        mioDiv.style.backgroundColor = "red";
+    mioDiv.style.backgroundColor = "red";
 
-        mioDiv.style.fontSize = "30px";
+    mioDiv.style.fontSize = "30px";
 
-        mioDiv.style.border = "2px solid black";
+    mioDiv.style.border = "2px solid black";
 
-        if (mioDiv.style.display === "none") {
-            mioDiv.style.display = "block";
-        } else {
-            mioDiv.style.display = "none";
-        }
+    if (mioDiv.style.display !== "none") {
+        mioDiv.style.display = "none";
+    } else {
+        mioDiv.style.display = "block";
+    }
 
-    })
+})
+
+function aggiornaContatore(){
+    let numeroElementi = listaVuota.children.length;
+    contatore.innerText = numeroElementi;   
+}
+
 
 
