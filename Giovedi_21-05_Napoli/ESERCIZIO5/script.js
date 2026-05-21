f1eda0fc809eeca03aa009479960114b74900f87
Kevin Napoli <kevinnapoli@outlook.it>
2026-05-21 15:02:44 +0200

    completato es5

--- Giovedi_21-05_Napoli/ESERCIZIO5/script.js
+++ Giovedi_21-05_Napoli/ESERCIZIO5/script.js
@@ -10,3 +10,29 @@ btnHamburger.addEventListener("click", function () {
     mainNav.classList.toggle("active");
 });
 
+
+bookForm.addEventListener("submit", function (event) {
+
+    event.preventDefault() // blocca il refresh 
+
+    const title = document.getElementById('book-title').value;
+    const author = document.getElementById('book-author').value;
+    const genere = document.getElementById('book-genere').value;
+    const year = document.getElementById('book-year').value;
+    const desc = document.getElementById('book-desc').value;
+
+
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+
+
+    bookCard.innerHTML = `
+         <h4>${title}</h4>
+         <div class="meta-info">${author} | Anno: ${year} | Genere: ${genere}</div>
+         <p>${desc}</p>
+         `;
+
+         bookContainer.appendChild(bookCard); //prende la card riempita e la mette nel contetnitore vistibile
+
+        bookForm.reset();
+})
